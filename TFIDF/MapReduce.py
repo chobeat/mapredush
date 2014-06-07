6d82bd86407df15aed0e4c062bd7ec910e33d219
GustavAdolf <simone.robutti@gmail.com>
2014-06-07 20:16:56 +0200

    La commit di prima era uno scherzo.

--- TFIDF/MapReduce.py
+++ TFIDF/MapReduce.py
@@ -7,16 +7,6 @@ import logging
 import re
 import string
 
-#Trasforma un documento in un dizionario di occorrenze delle parole presente. Fa una sanitizzazione della punteggiatura ma non toglie stop words
-def docToTF(doc):
-	valid_chars = " %s%s" % (string.ascii_letters, string.digits)
-	doc=''.join(c for c in doc if c in valid_chars)
-	words=doc.split(" ")
-	TF={}
-	for word in words:
-		TF[word]=TF.setdefault(word,0)+1
-	return TF
-
 def TFmap1(textField):
 	func=Code("""function(){
 		words=this."""+textField+""".split(' ')
@@ -64,7 +54,7 @@ if __name__=="__main__":
 	client=MongoClient()
 	db=client.test
 	cars=db.cars
-	#cars.map_reduce(TFmap1("_id"),TFreduce1(),"resultTF1")
+	cars.map_reduce(TFmap1("_id"),TFreduce1(),"resultTF1")
 
 	resultTF=db.resultTF1
 	resultTF.map_reduce(TFmap2(),TFreduce2(),"resultTF2")

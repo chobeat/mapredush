44db02e9ca27aeb8bd3213fc94e7f98824ffc6bb
ForliMWM <forlivesi@macrowebmedia.it>
2014-06-10 22:58:47 +0200

    Provato a cambiare l'implementazione di aggregate

--- mongocv/aggregate.py
+++ mongocv/aggregate.py
@@ -1,31 +1,54 @@
 from matchdocument import MatchDocument
 from groupdocument import GroupDocument
 from querydocument import QueryDocument
+from sortdocument import SortDocument
+from pymongo import MongoClient
 
 class Aggregate(list):
+
+    def __init__(self, mongo_document):
+        list.__init__(self)
+        self.mongo_document = mongo_document
 
     def append(self, x):
         doc = x.getdoc()
         list.append(self, doc)
 
+    def aggregate(self):
+        self.results = self.mongo_document.aggregate(self)
+
+    def printResults(self, fields=[]):
+        results = self.results["result"]
+
+        if fields==[]:
+            for r in results:
+                print r
+        else:
+            string = ""
+            for r in results:
+                for field in fields:
+                    string += field+": "+str(r[field])+" "
+                string += "\n"
+            print string
+
 
 """
 USE CASE
+cars = MongoClient()["dbtest"]["cars"]
 
-q = QueryDocument()
-q.setfield("year")
-q.addgt(2011)
-match = MatchDocument()
-match.addquerydoc(q)
-group = GroupDocument()
-group.setid("id")
-group.addsum("count",1)
-aggr = Aggregate()
-aggr.append(match)
-aggr.append(group)
-print aggr
+aggregate = Aggregate(cars)
+
+oldestMaker = GroupDocument()
+oldestMaker.setid("make")
+oldestMaker.addmax("oldestYear","year")
+
+aggregate += [oldestMaker.getdoc()]
+
+sortOldestMaker = SortDocument()
+sortOldestMaker.addfield("oldestYear")
+aggregate += [sortOldestMaker.getdoc()]
+
+aggregate.aggregate()
+
+aggregate.printResults(["_id","oldestYear"])
 """
-
-
-
-

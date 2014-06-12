d6793196379a2cce69062a133bcb17c58f496c7d
GustavAdolf <simone.robutti@gmail.com>
2014-06-12 16:05:32 +0200

    main tfidf inizializzatore

--- consegna/inizializzatore.py
+++ consegna/inizializzatore.py
@@ -1,6 +1,14 @@
 from pymongo import *
 from pymongo.errors import *
 from mongocv import *
+from mongocv.aggregate import Aggregate
+from mongocv.groupdocument import GroupDocument
+from mongocv.matchdocument import MatchDocument
+from mongocv.outdocument import OutDocument
+from mongocv.querydocument import QueryDocument
+from mongocv.document import Document
+
+
 
 def connToDB(collection):
 	client=MongoClient()
@@ -11,17 +19,27 @@ def initDB():
 	try:
 		db=connToDB("occurrences").database
 		print "Inizializzazione database"
-		init_IDF(db.wordIDF)
+		init_IDF()
 	except CollectionInvalid:
 		pass
 
-def init_IDF(coll):
-	db=connToDB("occurrences").database
-	db.wordIDF.drop()
-	db.create_collection("wordIDF")
-	occurrences=connToDB("occurrences")
-	num=occurrences.count()
-	print num
-	res=occurrences.aggregate()
+def init_IDF():
+
+    DB=connToDB("occurrences").database
+    DB.create_collection("wordIDF")
+    coll = DB["occurrences"]
+    den = DB["texts"].count()
+    group = GroupDocument("keyword")
+    group.addpush("tweets", "textid")
+    aggr = Aggregate()
+    project = {"$project": {"keyword": 1, "idf": {"$divide": [den, {"$size": "$tweets"}]}}}
+    out = OutDocument("wordIDF")
+
+    aggr.append(group.getdoc())
+    aggr.append(project)
+    aggr.append(out.getdoc())
+
+    coll.aggregate(aggr)
+
 
 initDB()
---
+++ consegna/main.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from pymongo.mongo_client import MongoClient
+from mongocv.aggregate import Aggregate
+from mongocv.groupdocument import GroupDocument
+from mongocv.matchdocument import MatchDocument
+from mongocv.outdocument import OutDocument
+from mongocv.querydocument import QueryDocument
+from mongocv.document import Document
+import math
+import sys
+__author__ = 'civi'
+
+DB = MongoClient()["contestDB_E"]
+
+def tweetid2words(tweetid):
+    coll = DB["occurrences"]
+    query = Document()
+    query.add("textid",tweetid)
+    match = MatchDocument(query)
+
+    group = GroupDocument("textid")
+    group.addpush("words", "keyword")
+
+    aggr = Aggregate()
+    aggr.append(match)
+    aggr.append(group)
+
+    res = coll.aggregate(aggr)
+    words = res["result"][0]["words"]
+    return words
+
+def getfreqdict(words):
+    den = len(words)
+    wordsVector = Counter(words)
+    freqdict = {word: wordsVector[word]/float(den) for word in wordsVector}
+    return freqdict
+
+def tweetid2freqdict(tweetid):
+    words = tweetid2words(tweetid)
+    return getfreqdict(words)
+
+def tfidf(tweetid):
+    res = dict()
+    coll = DB["wordIDF"]
+    tf = tweetid2freqdict(tweetid)
+    for word in tf:
+        query = Document()
+        query.add("_id", word)
+        idf = math.log(coll.find_one(query.getdoc())["idf"])
+        res[word] = tf[word]*idf
+    return res
+
+if __name__=="__main__":
+	pass
